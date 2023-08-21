d98a26c5f160468d3c0a09c71659c23120cec7ce
Austin Shih <austinhshih@gmail.com>
2023-08-21 13:59:57 -0700

    Added mixins

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,7 +1,7 @@
 import { Inter } from '@next/font/google';
 import { ReactElement } from 'react';
 
-import styles from '../styles/example/examples.module.scss'
+import styles from '../styles/example/example.module.scss'
 
 const inter = Inter({ subsets: ['latin'] });
 
