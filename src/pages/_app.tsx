97542521b1758136d41ca202b46dee6a566c4d7c
Ryan <ryyu444@gmail.com>
2023-09-05 20:42:37 -0700

    Updated next font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { ReactElement } from "react";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import '@/styles/globals.scss';
 
 const inter = Inter ({
