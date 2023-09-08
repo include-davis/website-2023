cf274170f23452a35d2f1dcc5eeab8ec4454f9b7
Ryan <ryyu444@gmail.com>
2023-09-07 21:14:59 -0700

    Slight changes for testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react";
 
 export default function Home(): ReactElement {
   return (
-    <div style={{ backgroundColor: "purple", padding: "250px 0px" }}>
+    <div style={{ backgroundColor: "purple", padding: "1000px 0px" }}>
       hello
       <div style={{ backgroundColor: "green" }}>
         <h1>Home</h1>
