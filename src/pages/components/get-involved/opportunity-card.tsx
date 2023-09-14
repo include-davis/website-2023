45a3247f2d462277eeac1df2accc773cd729a821
Samantha Iraheta <sairaheta1@gmail.com>
2023-09-14 15:27:34 -0700

    modified files and modified components to use props instead

--- src/pages/components/get-involved/opportunity-card.tsx
+++ src/pages/components/get-involved/opportunity-card.tsx
@@ -1,21 +1,20 @@
-import { ReactElement } from 'react';
+import { JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactNode, ReactPortal } from 'react';
 import styles from '../../../styles/get-involved/opportunity-card.module.scss';
 import Image from 'next/image';
 
-export default function OpportunityCard(): ReactElement {
+export default function OpportunityCard(props: { name: string; description: string; alt: string;}): ReactElement {
   return (
     <div className={styles.cardContainer}>
       <Image
-      src="/get-involved/svgs/pencil.svg"
+      src={`/get-involved/svgs/${props.name}.svg`}
       width={40}
       height={40}
-      alt="Icon of a pencil with ruler through it"
+      alt={props.alt}
       className={styles.icon}
       />
-      <h4 className={styles.header}>Design</h4>
+      <h4 className={styles.header}>{props.name}</h4>
       <p className={` ${styles.p} p-small`}>
-        Focused on visual design, user experience, and user interface.
-      </p>
+{props.description}</p>
       <button className={styles.button}>LEARN MORE</button>
     </div>
   );
--- src/pages/components/get-involved/opportunity-content.tsx
+++ src/pages/components/get-involved/opportunity-content.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import Image from 'next/image';
 import OpportunityCard from './opportunity-card';
 import styles from '../../../styles/get-involved/opportunity-section.module.scss'
+import data from '../get-involved/../../../json/get-involved.json';
+
 
 export default function Opportunities(): React.JSX.Element {
+  const cards = data.cards;
+  const cardElements = Object.keys(cards).map((index) => {
+    const card = cards[index as keyof typeof cards];
+    return (
+    <OpportunityCard name={card.header} description={card.description} alt={card.alt}></OpportunityCard>
+    );
+});
+
   return (
     <section className={styles.opportunityContainer}>
       <div className={styles.header}>
@@ -14,10 +24,7 @@ export default function Opportunities(): React.JSX.Element {
         alt="SuitCase with magnifying glass"/>
         <h3>Opportunities</h3>
       </div>
-      <div className={styles.cardsContainer}>
-        <OpportunityCard></OpportunityCard>
-        <OpportunityCard></OpportunityCard>
-        <OpportunityCard></OpportunityCard>
+      <div className={styles.cardsContainer}> {cardElements}
       </div>
     </section>
     )
