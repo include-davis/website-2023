43dca3253af26505f1efc030a5f5f04113a95963
Ryan <ryyu444@gmail.com>
2023-09-18 19:30:47 -0700

    Merged main, added footer spaces, & adjusted styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react";
 
 export default function Home(): ReactElement {
   return (
-    <div style={{ padding: "250px", backgroundColor: "grey" }}>
+    <div style={{ height: "100vh", backgroundColor: "grey" }}>
       empty homepage
       <div>hello!</div>
     </div>
