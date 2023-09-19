74c2d3c25f982dd03e21d2a141e5d781194ea187
Samantha Iraheta <sairaheta1@gmail.com>
2023-09-19 14:23:38 -0700

    commented things out to pass check

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,26 @@
 import '@/styles/globals.scss';
-import type { AppProps } from "next/app";
-import { ReactElement } from "react";
-import { Inter } from "@next/font/google";
-import { useRouter } from "next/router";
-import "../styles/globals.scss";
+import type { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+import { Inter } from '@next/font/google';
+// import { useRouter } from 'next/router';
 
 const inter = Inter({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  variable: "--font-inter",
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-inter',
 });
 
-const navLinks = [
-  { name: "About", path: "about" },
-  { name: "Projects", path: "projects" },
-  { name: "Get Involved", path: "get-involved" },
-];
+// const navLinks = [
+//   { name: 'About', path: 'about' },
+//   { name: 'Projects', path: 'projects' },
+//   { name: 'Get Involved', path: 'get-involved' },
+// ];
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
-  const router = useRouter();
+  // const router = useRouter();
 
   return (
-    <div className={`${inter.variable}`} style={{ position: "relative" }}>
+    <div className={`${inter.variable}`} style={{ position: 'relative' }}>
       <Component {...pageProps} />
     </div>
   );
