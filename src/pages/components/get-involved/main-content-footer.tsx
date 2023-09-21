970b7e98ee881ebc6001ab8cb5721286e9b86194
Samantha Iraheta <sairaheta1@gmail.com>
2023-09-20 18:54:54 -0700

    changed paddings to include global edge spacer

--- src/pages/components/get-involved/main-content-footer.tsx
+++ src/pages/components/get-involved/main-content-footer.tsx
@@ -5,7 +5,7 @@ export default function MainContentFooter(): ReactElement {
   return (
     <section className={styles.mainFooter}>
       <h2 className={styles.question}>
-        Are you a company looking to build<br></br>something amazing?
+        Are you a company looking to build something amazing?
       </h2>
       <p className={styles.small_text}>Explore what we can do for you</p>
       <button type="button" className={styles.btn}>
