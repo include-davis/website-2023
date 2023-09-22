c8380edb33921cc2d4e3219fb0b734716dd25cb7
ylylime <eming@ucdavis.edu>
2023-09-22 11:00:04 -0700

    some css

--- src/pages/components/homepage/header.tsx
+++ src/pages/components/homepage/header.tsx
@@ -5,25 +5,28 @@ import styles from '../../../styles/homepage/header.module.scss';
 export default function Header(): ReactElement {
     return(
         <div className={styles.homepage}>
+        
+        <div className={styles.main_content}>
 
-        <div className={styles.images}>
+          <div className={styles.images}>
             <img width="400" height="200" src="/homepage/design-home1.png" />
             <img width="400" height="200" src="/homepage/design-home2.png" />
             <img width="400" height="200" src="/homepage/design-home3.png" />
-        </div>
+          </div>
 
-        <div className={styles.main_content}>
           <h1 className={styles.title}>
             <span className={styles.diagonal}>Design.</span>
             <span className={styles.diagonal}>Develop.</span>
             <span className={styles.diagonal}>Create.</span>
           </h1>
+
+          <p className={styles.description}>We are a UC Davis community of web developers and designers dedicated to fostering collaboration, growth and the creation of creative digital solutions.</p>
+          
+          <button className={styles.join}>Join the community</button>
+          
         </div>
 
-          <p>We are a UC Davis community of web developers and designers dedicated to fostering collaboration, growth and the creation of creative digital solutions.</p>
-          <button className={styles.join}>Join the community</button>
-
-        <div className={styles.titlepg_footer}>
+        <div className={styles.footer}>
           <p>Where we've gone</p>
 
         </div>
