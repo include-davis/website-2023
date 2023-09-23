eb049ffe0a35d73b419e98f462ef43b8e3d8c9ed
ylylime <eming@ucdavis.edu>
2023-09-23 16:35:33 -0700

    recent work init

--- src/pages/components/homepage/recent-work.tsx
+++ src/pages/components/homepage/recent-work.tsx
@@ -1,10 +1,11 @@
 import { ReactElement } from 'react';
-//import styles from '../../../styles/homepage/header.module.scss';
+import styles from '../../../styles/homepage/header.module.scss';
 
 export default function Work(): ReactElement {
     return(
         <div>
-        
+            <h3 className={styles.title}>Recent Work</h3>
+
         </div>
     );
 
