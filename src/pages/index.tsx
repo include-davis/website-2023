ed567a230de8f17ab24e0a537f396358b5149dbf
Austin Shih <austinhshih@gmail.com>
2023-09-26 08:13:06 -0700

    fixed a bunch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,5 +10,32 @@ import HomeFooter from './components/homepage/footer';
 import styles from '../styles/homepage/homepage.module.scss';
 
 export default function Home(): ReactElement {
-  return <div>empty homepage</div>;
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.header} >
+        <HomeHeader />
+      </div>
+      <div className={styles.grow}>
+        <Grow />
+      </div>
+
+      <div className={styles.purple}>
+        <div className={styles.offset} >
+          <div className={styles.mission} >
+            <Mission />
+          </div>
+          <div className={styles.work} >
+            <RecentWork />
+          </div>
+          <div className={styles.pillars} >
+            <Pillars />
+          </div>
+          <div className={styles.footer} >
+            <HomeFooter />
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
 }
