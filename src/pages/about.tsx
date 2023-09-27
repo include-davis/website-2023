e47960f5c4aec59ba312a7e3cc4236b0dd607e66
Samantha Iraheta <sairaheta1@gmail.com>
2023-09-26 20:44:02 -0700

    Feat/get involved (#236)
    * finished bottom content of page
    
    * finished basic layout of opportunity cards
    
    * added necessary svgs
    
    * finished final layout of opportunities
    
    * added json folder and info for my page
    
    * modified files and modified components to use props instead
    
    * basic template of top of webpage w/o any animated portions implemented
    
    * added panel graphics
    
    * updated json data
    
    * finished template of component
    
    * finished scroller
    
    * finsihed scroller animation
    
    * finsihed scroll animation for bullet points
    
    * finished image placement
    
    * added key to elimante console log error
    
    * finally made eslint happy
    
    * commented things out to pass check
    
    * styling issues fixed
    
    * modified props
    
    * changed paddings to include global edge spacer
    
    * moved everything into get-involved.tsx
    
    * deleted files no longer needed
    
    * added responsive design
    
    * changed to react count up
    
    * finished sizing issues for responsive design
    
    * fixed responsive issues
    
    * added padding for footer/header
    
    * fixed overflow and number card size change bug
    
    ---------
    
    Co-authored-by: Austin Shih <austinhshih@gmail.com>

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -21,16 +21,18 @@ let companies = [
   'Netflix',
 ];
 
-companies = [...companies, ...companies];
-const companiesContent = companies.map((company, index) => (
-  <img
-    src={`/about/svgs/${company}.svg`}
-    loading="eager"
-    alt=""
-    key={`${company} ${index}`}
-  />
-));
-
+companies = companies.concat(companies.slice(0));
+const companiesContent = companies.map((company, index) => {
+  return (
+    <img
+      src={`/about/svgs/${company}.svg`}
+      loading="eager"
+      alt=""
+      key={`${company} ${index}`}
+    />
+  );
+});
+ 
 const blurs = {
   blue: '/about/svgs/blurBlue.svg',
   purple: '/about/svgs/blurPurple.svg',
