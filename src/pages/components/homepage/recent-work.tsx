ebc69be139a935399397bfbc3a7b6ac0870fcac7
Emily Ng <96224511+ylylime@users.noreply.github.com>
2023-10-08 21:04:59 -0700

    Homepage mobile fix (#267)
    * fix mobile padding
    
    * margin fix
    
    * tweaked padding, added png
    
    ---------
    
    Co-authored-by: Austin Shih <austinhshih@gmail.com>

--- src/pages/components/homepage/recent-work.tsx
+++ src/pages/components/homepage/recent-work.tsx
@@ -23,7 +23,7 @@ export default function Work(): React.JSX.Element {
   return (
     <div className={styles.work}>
       <div className={styles.title}>
-        <img src="https://s3-alpha-sig.figma.com/img/edec/10a6/814feda96c73bedf4cf97d5868202e29?Expires=1696809600&Signature=huPBh-vLqLe2-YsiGphnWR6OivZsTJHFNtULZ7UkQ5cM58uRHXeet73JXhVMjcFO0R6O851woSmSiVU582Fy0Cl4-1ocJdscuzx-wbvn0WTJ~gkZRtqYrhD5rSyAjkgIylaVSMgxOWN0eKWzEpadQiLzBZqOWjkKpRngdt1VBpNW7QTSIx2DFvWrl~IAu6wEh6ja75Wg4YzJydkfXgcEhD-J48jRJ1HbY5BHbjl7oECmRFexIomtAjO5gHpvLsLPwutrzYpwBcu-Ffuu2xODVLzEng5yBKYBxB1ymnOiU4CYLQBpOhodBZ3M4YcdGvqgdnRoSCbPEBQDMTxWkFpHFQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+        <img src="/homepage/pen.png" />
         <h3>Recent Work</h3>
         <p>Transforming ideas into incredible realities is what we do best.</p>
       </div>
